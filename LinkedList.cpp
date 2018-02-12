8372a40098172dfa672899fa1781a93772158d9c
unknown <akash.jawla1995@gmail.com>
2018-02-12 23:19:52 +0530

    final

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,20 +1,15 @@
 #include<iostream>
-
 using namespace std;
-
 class Node{
   public:
   int data;
   Node *next;
-
   Node(int n){
     data=n;
     next=NULL;
   }
 };
-
 Node* head = NULL;
-
 void display(){
   Node* n=head;
   cout<<"List -> ";
@@ -23,7 +18,6 @@ void display(){
     n=n->next;
   }
 }
-
 void insert_beg(int data){
   Node* n = new Node(data);
   if(head==NULL){
@@ -34,7 +28,6 @@ void insert_beg(int data){
     head=n;
   }
 }
-
 void insert_end(int data){
   Node* n=new Node(data);
   if(head==NULL){
@@ -48,7 +41,6 @@ void insert_end(int data){
     f->next=n;
   }
 }
-
 void insert_any(int data,int pos){
   if(pos==1) {insert_beg(data); return;}
   Node* n=new Node(data);
@@ -62,13 +54,11 @@ void insert_any(int data,int pos){
   n->next=f->next;
   f->next=n;
 }
-
 void del_beg(){
   Node* n=head;
   head=n->next;
   free(n);
 }
-
 void del_end(){
   Node* f=head;
   Node* n=f;
@@ -80,33 +70,59 @@ void del_end(){
   n->next=NULL;
   free(f);
 }
-
+void del_any(int pos){
+    if(pos==1){del_beg(); return;}
+    if(head==NULL){return;}
+    Node* f=head;
+    for(int i=0;i<pos-2 && f!=NULL;i++){
+    f=f->next;
+    }
+    Node* n=f->next;
+    f->next=n->next;
+    free(n);
+}
 int main() {
   int data;
-  while (1) {
-  cout<<"\nSelect option\n";
-  cout<<"1. insert Beg\n";
-  cout<<"2. insert Any\n";
-  cout<<"3. insert End\n";
-  cout<<"4. delete Beg\n";
-  cout<<"5. delete End\n";
-  cout<<"7. display\n";
   int option;
-  Start1:
+  int pos;
+  do {
+  cout<<"\nSelect option\n";
+  cout<<"1. Insert\n";
+  cout<<"2. Delete\n";
+  cout<<"3. Display\n";
+  cout<<"4. Exit\n";
+
+
   cin>>option;
-  if(option==1 || option==2 || option==3){
-    cout<<"Enter data : ";
-    cin>>data;
-  }
+  if(option==1){
+      cout<<"1. Insert in the Beggining.\n";
+      cout<<"2. Insert at any Position.\n";
+      cout<<"3. Insert in the End.\n";
+      cin>>option;
+      cout<<"Enter data : ";
+      cin>>data;
+      }
+  else if(option==2){
+      cout<<"1. Delete in the Beggining.\n";
+      cout<<"2. Delete in the End.\n";
+      cout<<"3. Delete at any Position.\n";
+      cin>>option;
+      option+=3;
+      }
+   else if(option==3) option=7;
+    else if(option==4) option=8;
+    else option=9;
   switch (option) {
     case 1: insert_beg(data); break;
-    case 2: cout<<"Enter position : "; int pos; cin>>pos; insert_any(data,pos); break;
+    case 2: cout<<"Enter position : "; cin>>pos; insert_any(data,pos); break;
     case 3: insert_end(data); break;
     case 4: del_beg(); break;
     case 5: del_end(); break;
+    case 6: cout<<"Enter position : "; cin>>pos; del_any(pos); break;
     case 7: display(); break;
+    case 8: break;
     default: cout<<"Wrong input. Enter again!!\n";
   }
-}
+}while(option!=8);
   return 0;
 }
